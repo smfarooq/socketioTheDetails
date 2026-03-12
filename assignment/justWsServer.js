e35b95690cee159952e85bc5e35dafb3cbcd6188
Muhammad Farooq <muhammad.farooq@avanzasolutions.com>
2026-03-12 17:43:03 +0500

    working on messages

--- assignment/justWsServer.js
+++ assignment/justWsServer.js
@@ -6,6 +6,8 @@ const http_server = http.createServer((req, res) => {    // this is the HTTP ser
 });
 const wss = new ws.Server({ server:http_server }); // this creates a WebSocket server that shares the same HTTP server
 wss.on('headers', (headers) => { // this event is triggered when the WebSocket server is ready to send headers to the client
+  //handshake headers are sent to the client when a new WebSocket connection is established, 
+  //and this event allows you to modify or log those headers before they are sent
   console.log(headers);
   console.log('WebSocket server is ready to send headers');
 });
@@ -13,6 +15,14 @@ wss.on('connection', (socket, req) => { // this event is triggered when a new We
   console.log('A new client has connected');
   socket.send('Welcome to the WebSocket server!!!'); // send a welcome message to the client
   console.log(req);
+  socket.on('message', (message) => { // this event is triggered when a message is received from the client
+    console.log('Received message from client: ' + message);
+    //Caution! create a loop back effect if you send a message back to the client in response to a message received from the client, 
+    // and the client is programmed to send a message back to the server in response to any message received from the server. 
+    // This can lead to an infinite loop of messages being sent back and forth between the client and server, which can cause performance issues and potentially crash the server or client. 
+    //socket.send('Server received your message: ' + message); // Don't do this if the client is programmed to respond to any message from the 
+    // server, as it will create a loop back effect
+  });
 });
 http_server.listen(8080); // the server will listen on port 8080
 console.log('HTTP server is listening on port 8080');
