d89dbc2fa6c8055e4d4d9187d57d663c54b7351b
Muhammad Farooq <muhammad.farooq@avanzasolutions.com>
2026-03-12 16:40:53 +0500

    assignment, client and server

--- assignment/justWsServer.js
+++ assignment/justWsServer.js
@@ -5,5 +5,14 @@ const http_server = http.createServer((req, res) => {    // this is the HTTP ser
   res.end('Hello, this is a WebSocket server!');
 });
 const wss = new ws.Server({ server:http_server }); // this creates a WebSocket server that shares the same HTTP server
+wss.on('headers', (headers) => { // this event is triggered when the WebSocket server is ready to send headers to the client
+  console.log(headers);
+  console.log('WebSocket server is ready to send headers');
+});
+wss.on('connection', (socket, req) => { // this event is triggered when a new WebSocket connection is established
+  console.log('A new client has connected');
+  socket.send('Welcome to the WebSocket server!!!'); // send a welcome message to the client
+  console.log(req);
+});
 http_server.listen(8080); // the server will listen on port 8080
 console.log('HTTP server is listening on port 8080');
